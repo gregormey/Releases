43f19dcbc32af4ca2284757408bfa241587d789d
Gregor Meyenberg <gregor@meyenberg.de>
2013-07-15 10:13:19 +0200

    Initial Commit

---
+++ Releases/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Releases
+//
+//  Created by Gregor Meyenberg on 15.07.13.
+//  Copyright (c) 2013 Gregor Meyenberg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
