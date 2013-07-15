a56c3d87af0fea5886d50d157ec5423ce82a86b2
Gregor Meyenberg <gregor@gregors-imac.speedport_w723_v_typ_a_1_00_096>
2013-07-15 22:44:46 +0200

    add unit test to project

---
+++ TestReleases/TestReleases.h
@@ -0,0 +1,13 @@
+//
+//  TestReleases.h
+//  TestReleases
+//
+//  Created by Gregor Meyenberg on 15.07.13.
+//  Copyright (c) 2013 Gregor Meyenberg. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestReleases : SenTestCase
+
+@end
---
+++ TestReleases/TestReleases.m
@@ -0,0 +1,32 @@
+//
+//  TestReleases.m
+//  TestReleases
+//
+//  Created by Gregor Meyenberg on 15.07.13.
+//  Copyright (c) 2013 Gregor Meyenberg. All rights reserved.
+//
+
+#import "TestReleases.h"
+
+@implementation TestReleases
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestReleases");
+}
+
+@end
